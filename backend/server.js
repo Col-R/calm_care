333b32c02bf8a8bf9c9b5393cfbadc329a03fa97
Col-R <2colewestin@gmail.com>
2022-05-27 12:30:07 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// node backend/server.js to check if server is up and running
