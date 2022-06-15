a9d8cafbef6f38c5dfb0bfdba794f260f81b5a58
Col-R <2colewestin@gmail.com>
2022-06-15 14:43:35 -0500

    continued work on backend

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Initial commit - 5/27/22
 Making this as an e-commerce site for my local massage place. 
 See package.json for dependency stuff.
 
-once I get the backend set up, I'll add a script to start the whole thing. Probably npm run dev
+once I get the backend set up, I'll add a script to start the whole thing. 
+
+npm run dev to use
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 
-
+// express app
 const app = express();
 
-app.use(express.json());
+// middleware
+app.use((req, res, next)=> {
+    console.log(req.path, req.method);
+    next()
+})
 
+// route
+app.get('/', (req, res) => {
+    res.json({message: "Welcome to the app"})
+})
+
+// request listener
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-// node backend/server.js to check if server is up and running
+// nodemon backend/server.js to check if server is up and running
