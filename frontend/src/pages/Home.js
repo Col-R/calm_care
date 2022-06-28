2920e3871c70c23f1c5127470225a2c8275aa698
Col-R <2colewestin@gmail.com>
2022-06-28 11:31:55 -0500

    began work on front end, installed react dependencies

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Home</h2>
+        </div>
+    )
+}
+
+export default Home
