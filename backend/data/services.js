d16fc2307b683cb142db54fef8ab38b19419f7fd
Col-R <2colewestin@gmail.com>
2022-07-07 10:08:13 -0500

    data seeder script done

--- backend/data/services.js
+++ backend/data/services.js
@@ -1,20 +1,20 @@
 const services = [
     {
-        name: 'Individual 60-minute Massage',
+        name: "Individual 60-minute Massage",
         price: 125
     },
     {
-        name: 'Individual 90-minute Massage',
+        name: "Individual 90-minute Massage",
         price: 190
     },
     {
-        name: 'Couples 60-minute VIP massage',
+        name: "Couples 60-minute VIP massage",
         price: 250
     },
     {
-        name: 'Couples 90-minute VIP massage',
+        name: "Couples 90-minute VIP massage",
         price: 380
-    }
+    },
 ];
 
 module.exports = services;
