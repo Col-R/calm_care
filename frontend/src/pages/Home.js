ce492df01b965f1b1dcd4054ef583071c22858f4
Col-R <2colewestin@gmail.com>
2022-07-08 11:20:15 -0500

    data displaying to page, established proxy server to avoid CORS issues

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 //components
 import ServiceDetails from '../components/ServiceDetails';
 
 const Home = () => {
-    return <div className="homepage">Home Page</div>
+  const [services, setServices] = useState(null)
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      const response = await fetch('/api/service/services')
+      const json = await response.json()
+
+      if (response.ok) {
+        setServices(json)
+      } else {
+        console.log('response not ok')
+      }
+    }
+
+    fetchServices()
+  }, [])
+
+    return (
+    <div className="homepage">
+      {services && services.map((service) => (
+        <p key={service._id}>{service.name}</p>
+      ))}
+    </div>
+    )
 }
   
-  export default Home;
+export default Home;
