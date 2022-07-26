e818a2b49d63c2c04b52ff026fe92f78ba156521
Col-R <2colewestin@gmail.com>
2022-07-26 10:20:50 -0500

    navbar background pattern

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,7 +22,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className = "navbar">
-            <img src="https://images.unsplash.com/photo-1484291470158-b8f8d608850d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" id = "nav__img"/>
+
             <div className="navbar__logo">
                 <h2>Calm Care Massage</h2>
             </div>
