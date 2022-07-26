3d7bbb69f50324f0924b7b984310a2a4faff628c
Col-R <2colewestin@gmail.com>
2022-07-26 12:35:54 -0500

    finished nav link item styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -26,14 +26,18 @@ const Navbar = () => {
             <div className="navbar__logo">
                 <h2>Calm Care Massage</h2>
             </div>
-                <ul>
-                    <li class="nav_link_item">
-                        <Link to = "/">Home</Link>
-                    </li>
-                
-                    <li class="nav_link_item">
-                        <Link to = "/services">Services</Link>
-                    </li>
+                <ul className = "navbar__links">
+                    <Link to = "/" className = "nav_link">
+                        <li className="nav_link_item">
+                            Home
+                        </li>
+                    </Link>
+                    
+                    <Link to = "/services" className = "nav_link">
+                        <li className="nav_link_item">
+                            Services
+                        </li>
+                    </Link>
                 </ul>
         </nav>
     )
